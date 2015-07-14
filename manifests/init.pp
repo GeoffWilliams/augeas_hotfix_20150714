9afa9edce0fb65579f624ae8c560cc8d59e289d5
Geoff Williams <geoff.williams@puppetlabs.com>
2015-07-14 15:08:58 +1000

    documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,19 @@
 # == Class: augeas_hotfix_20150714
 #
-# Full description of class augeas_hotfix_20150714 here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# Install a fixed augeas lens for /etc/sudoers when included.  This addresses
+# https://github.com/hercules-team/augeas/issues/262
 #
 # === Examples
 #
-#  class { 'augeas_hotfix_20150714':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+#  include augeas_hotfix_20150714
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Name <geoff.williams@puppetlabs.com>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Puppet Labs, unless otherwise noted.
 #
 class augeas_hotfix_20150714 {
   file { "/opt/puppet/share/augeas/lenses/dist/tests/test_sudoers.aug":
